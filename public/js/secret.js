2ab1020f066235c5446660c6b2485e2b0ee7d42a
Vivaan Saroj <149756428+immortalvivaan461@users.noreply.github.com>
2025-07-29 22:45:25 +0530

    Update secret.js

--- public/js/secret.js
+++ public/js/secret.js
@@ -2,3 +2,15 @@ function toggleEditForm() {
     const form = document.getElementById("editForm");
     form.style.display = (form.style.display === "none") ? "block" : "none";
 }
+
+function toggleEdit(sid) {
+    const form = document.getElementById(`edit-form-${sid}`);
+    const contentDiv = document.getElementById(`content-${sid}`);
+    if (form.style.display === "none") {
+      form.style.display = "block";
+      contentDiv.style.display = "none";
+    } else {
+      form.style.display = "none";
+      contentDiv.style.display = "block";
+    }
+  }
