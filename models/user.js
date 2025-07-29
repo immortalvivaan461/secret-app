a5809f46133c5dc4759b3dd3e9c46e26b7dfd7d3
Vivaan Saroj <149756428+immortalvivaan461@users.noreply.github.com>
2025-07-29 22:32:11 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -16,7 +16,12 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    secrets: [String]
+    secrets: [{
+    _id: false,
+    sid: { type: mongoose.Types.ObjectId, default: () => new mongoose.Types.ObjectId() },
+    content: String
+}]
+
 })
 
 
